127cd8f020242661abbc4c94f616fb9f7defd12d
PinsaraPerera <1pawanpinsara@gmail.com>
2024-12-07 00:31:30 +0530

    leaderboard redux slice updated

--- src/store/features/leaderboard/leaderboard.slice.ts
+++ src/store/features/leaderboard/leaderboard.slice.ts
@@ -5,12 +5,16 @@ interface LeaderBoardState {
   contributors: ContributorInterface[];
   repos: RepositoryInterface[];
   selectedRepo: string;
+  loadingContributors: boolean;
+  loadingRepos: boolean;
 }
 
 const initialState: LeaderBoardState = {
   contributors: [],
   repos: [],
   selectedRepo: "overall",
+  loadingContributors: false,
+  loadingRepos: false,
 };
 
 const leaderboardSlice = createSlice({
@@ -19,16 +23,25 @@ const leaderboardSlice = createSlice({
   reducers: {
     setContributors(state, action: PayloadAction<ContributorInterface[]>) {
       state.contributors = action.payload;
+      state.loadingContributors = false;
     },
     setRepos(state, action: PayloadAction<RepositoryInterface[]>) {
       state.repos = action.payload;
+      state.loadingRepos = false;
     },
     setSelectedRepo(state, action: PayloadAction<string>) {
       state.selectedRepo = action.payload;
     },
+    setLoadingContributors(state, action: PayloadAction<boolean>) {
+      state.loadingContributors = action.payload;
+    },
+    setLoadingRepos(state, action: PayloadAction<boolean>) {
+      state.loadingRepos = action.payload;
+    },
   },
 });
 
-export const { setContributors, setRepos, setSelectedRepo } = leaderboardSlice.actions;
+export const { setContributors, setRepos, setSelectedRepo } =
+  leaderboardSlice.actions;
 
 export default leaderboardSlice.reducer;
