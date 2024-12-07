d2d1cd6420636856afc3c29a304c4c45eb166cac
PinsaraPerera <1pawanpinsara@gmail.com>
2024-12-07 13:09:59 +0530

    optimize rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 import "../styles/globals.css";
 
-import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Provider } from "react-redux";
@@ -9,19 +8,16 @@ import { store } from "@/store/store";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
-// export const metadata: Metadata = {
-//   title: "Foss UOK official Website",
-//   description: "This is the official website of Foss UOK",
-// };
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   let persistor = persistStore(store);
+
   return (
     <html lang="en">
+      <head>
+        <title>Foss UOK official Website</title>
+        <meta name="description" content="This is the official website of Foss UOK" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="bg-gradient-to-b from-[#020c1b] via-[#0d1b2a] to-[#112240] text-gray-600 m-[40px]">
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
