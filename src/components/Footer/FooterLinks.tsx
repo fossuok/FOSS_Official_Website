e96c40e001c894740fa692e1de8b0cf87d7b3f63
SahanHeshan <dshsamarathunga@gmail.com>
2025-01-28 13:34:57 +0530

    added new data file

--- src/components/Footer/FooterLinks.tsx
+++ src/components/Footer/FooterLinks.tsx
@@ -8,36 +8,7 @@ import {
 import { ActionIcon, Container, Group, Text } from "@mantine/core";
 import { ThemedLogo } from "../ThemePicker/Theming";
 import classes from "./FooterLinks.module.css";
-
-const data = [
-  {
-    title: "About",
-    links: [
-      { label: "Features", link: "#" },
-      { label: "Pricing", link: "#" },
-      { label: "Support", link: "#" },
-      { label: "Forums", link: "#" },
-    ],
-  },
-  {
-    title: "Project",
-    links: [
-      { label: "Contribute", link: "#" },
-      { label: "Media assets", link: "#" },
-      { label: "Changelog", link: "#" },
-      { label: "Releases", link: "#" },
-    ],
-  },
-  {
-    title: "Community",
-    links: [
-      { label: "Join Discord", link: "#" },
-      { label: "Follow on Twitter", link: "#" },
-      { label: "Email newsletter", link: "#" },
-      { label: "GitHub discussions", link: "#" },
-    ],
-  },
-];
+import { data } from "@/data/FooterData";
 
 export function FooterLinks() {
   const groups = data.map((group) => {
