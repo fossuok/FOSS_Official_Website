22024ca20697672b72cd2838c7c98b0db5e3c406
UdasriHasindu <usrimart@gmail.com>
2025-01-31 19:05:45 +0530

    wrap components, add functionalities

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,4 +1,3 @@
-import { GetInTouch } from "./GetInTouch";
 import { Container } from "@mantine/core";
 import Section1 from "./section1";
 
@@ -6,9 +5,6 @@ export default function Home() {
   return (
     <div>
       <Section1 />
-      <Container size={900}>
-        <GetInTouch />
-      </Container>
     </div>
   );
 }
--- src/app/(pages)/about/section1.tsx
+++ src/app/(pages)/about/section1.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AboutCard from "./AboutCard";
+import { GetInTouch } from "./GetInTouch";
 import classes from "./section1.module.css";
 import { Button, Container, Group, Text } from "@mantine/core";
 
@@ -43,34 +44,16 @@ export default function Section1() {
 
       <div className={classes.wrapper}>
         <Container size={1000} className={classes.inner}>
-          <h1 className={classes.subtitle}>
-          About FOSSUOK
-          </h1>
-
-        <AboutCard/>
-
-
-{/* 
-          <Group className={classes.controls}>
-            <Button
-              size="xl"
-              component="a"
-              onClick={scrollToContact}
-              className={classes.control}
-              variant="gradient"
-              gradient={{ from: "violet", to: "grape" }}
-            >
-              Contact Us!
-            </Button>
-
-            <Text className={classes.description}>
-              We are a community of students who are passionate about Free and
-              Open Source Software. We are dedicated to promoting the use of
-              FOSS and contributing to the FOSS community.
-            </Text>
-          </Group> */}
+          <h1 className={classes.subtitle}>About FOSSUOK</h1>
+          <AboutCard />
         </Container>
       </div>
+
+      {/* <div className={classes.wrapper}> */}
+      <Container size={1000} className={classes.inner}>
+        <GetInTouch />
+      </Container>
+      {/* </div> */}
     </>
   );
 }
