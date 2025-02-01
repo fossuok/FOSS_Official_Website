7076a12e7125e956af87da928e9c18c05f03a720
SahanHeshan <dshsamarathunga@gmail.com>
2025-02-01 21:44:00 +0530

    updated about page styles

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,10 +1,14 @@
 import { Container } from "@mantine/core";
 import Section1 from "./section1";
+import AboutCard from "./AboutCard";
+import { GetInTouch } from "./GetInTouch";
 
-export default function Home() {
+export default function About() {
   return (
     <div>
       <Section1 />
+      <AboutCard />
+      <GetInTouch />
     </div>
   );
 }
