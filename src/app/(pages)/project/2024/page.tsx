53d979dbe61a4485193196e7554dbc0849894110
pathummadhusanka <commits@pathummadhusanka.com>
2025-03-30 09:41:59 +0530

    feat(summit-24): add speakers section

--- src/app/(pages)/project/2024/page.tsx
+++ src/app/(pages)/project/2024/page.tsx
@@ -6,6 +6,7 @@ import GradientBack from "@/components/Gradient/GradientBack";
 import { NavbarTimeline } from "../timeline";
 
 const resourceUrl = "/2024";
+import speakerList from "./speakerList";
 
 import { Metadata } from "next";
 
@@ -158,6 +159,49 @@ export default function Summit24() {
 							</div>
 						</div>
 					</div>
+				</div>
+			</Container>
+
+			<Container size={1200}>
+				<h2 className={classes.speakerSectionTitle}>Speakers</h2>
+				<div className={classes.speakers}>
+					{speakerList.map((speaker) => {
+						return (
+							<div
+								className={classes.speakerCard}
+								key={speaker.slug}
+							>
+								<div className={classes.speakerImgContainer}>
+									<img
+										className={classes.speakerImg}
+										src={`${resourceUrl}${speaker.image}`}
+										alt={`${speaker.name}`}
+									/>
+									<div className={classes.speakerLink}>
+										<a href={speaker.linkedin}>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												className={
+													classes.speakerLinkedin
+												}
+												viewBox="-4 0 52 52"
+											>
+												<title>LinkedIn</title>
+												<path d="M8.421 14h.052 0C11.263 14 13 12 13 9.5 12.948 6.945 11.263 5 8.526 5 5.789 5 4 6.945 4 9.5 4 12 5.736 14 8.421 14zM4 17H13V43H4zM44 26.5c0-5.247-4.253-9.5-9.5-9.5-3.053 0-5.762 1.446-7.5 3.684V17h-9v26h9V28h0c0-2.209 1.791-4 4-4s4 1.791 4 4v15h9C44 43 44 27.955 44 26.5z" />
+											</svg>
+										</a>
+									</div>
+								</div>
+
+								<span className={classes.speakerName}>
+									{speaker.name}
+								</span>
+								<span className={classes.speakerTitle}>
+									{speaker.title} at {speaker.company}
+								</span>
+							</div>
+						);
+					})}
 				</div>
 			</Container>
 
