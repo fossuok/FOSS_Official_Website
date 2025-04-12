e68cfa5149fbc16e2be944174ed102a4e19a9abe
SahanHeshan <dshsamarathunga@gmail.com>
2025-04-12 07:57:41 +0530

    added timeline
    (need to separate it)

--- src/app/(pages)/project/2025/page.tsx
+++ src/app/(pages)/project/2025/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Container, Group, Button } from "@mantine/core";
+import { Container, Group, Button, Timeline, Text } from "@mantine/core";
 import ImgGrid from "./ImgGrid";
 import GradientBack from "@/components/Gradient/GradientBack";
 
@@ -26,13 +26,6 @@ export default function Summit25() {
               Open-Dev Summit '25 <br />
               at University of Kelaniya
             </h1>
-
-            <Group justify="center" className={classes.controls}>
-              <p className={classes.time}>
-                May 24<sup>th</sup> 2025 <br />
-                University of Kelaniya, Sri Lanka
-              </p>
-            </Group>
           </div>
           <Group
             style={{
@@ -74,8 +67,127 @@ export default function Summit25() {
               Events
             </Button>
           </Group>
+
+          <Group justify="center" className={classes.controls}>
+            <Container py={100} size={400}>
+              <Timeline
+                color="teal"
+                active={0}
+                reverseActive
+                bulletSize={24}
+                align="left"
+              >
+                <Timeline.Item title="Workshop 1" className={classes.time}>
+                  <Text c="dimmed" size="md">
+                    Linux Fundamentals{" "}
+                  </Text>
+                  <Text>Week 1 - April 26th, 2025</Text>
+                  <Button
+                    disabled
+                    mt={20}
+                    size="md"
+                    radius="0 50 50 0"
+                    component="a"
+                    href={`/project/2025`}
+                    target="_blank"
+                  >
+                    Registrations - Coming Soon
+                  </Button>
+                </Timeline.Item>
+
+                <Timeline.Item title="Workshop 2" className={classes.time}>
+                  <Text c="dimmed" size="md">
+                    FOSS Tools for Big Data & AI{" "}
+                  </Text>
+                  <Text>Week 2 - May 3rd, 2025</Text>
+                  <Button
+                    disabled
+                    mt={20}
+                    size="md"
+                    radius="0 50 50 0"
+                    component="a"
+                    href={`/project/2025`}
+                    target="_blank"
+                  >
+                    Registrations - Coming Soon
+                  </Button>
+                </Timeline.Item>
+
+                <Timeline.Item title="Workshop 3" className={classes.time}>
+                  <Text c="dimmed" size="md">
+                    Building Modern Web Apps with .NET{" "}
+                  </Text>
+                  <Text>Week 3 - May 6th, 2025</Text>
+                  <Button
+                    disabled
+                    mt={20}
+                    size="md"
+                    radius="0 50 50 0"
+                    component="a"
+                    href={`/project/2025`}
+                    target="_blank"
+                  >
+                    Registrations - Coming Soon
+                  </Button>
+                </Timeline.Item>
+
+                <Timeline.Item title="Workshop 4" className={classes.time}>
+                  <Text c="dimmed" size="md">
+                    Mobile Application Development{" "}
+                  </Text>
+                  <Text>Week 3 - May 10th, 2025</Text>
+                  <Button
+                    disabled
+                    mt={20}
+                    size="md"
+                    radius="0 50 50 0"
+                    component="a"
+                    href={`/project/2025`}
+                    target="_blank"
+                  >
+                    Registrations - Coming Soon
+                  </Button>
+                </Timeline.Item>
+                <Timeline.Item title="Workshop 5" className={classes.time}>
+                  <Text c="dimmed" size="md">
+                    Cloud Native Development{" "}
+                  </Text>
+                  <Text>Week 4 - May 17th, 2025</Text>
+                  <Button
+                    disabled
+                    mt={20}
+                    size="md"
+                    radius="0 50 50 0"
+                    component="a"
+                    href={`/project/2025`}
+                    target="_blank"
+                  >
+                    Registrations - Coming Soon
+                  </Button>
+                </Timeline.Item>
+                <Timeline.Item title="SUMMIT DAY" className={classes.time}>
+                  <Text c="dimmed" size="md">
+                    at University of Kelaniya, Sri Lanka{" "}
+                  </Text>
+
+                  <Text>Week 5 - May 24th, 2025</Text>
+                  <Button
+                    mt={20}
+                    size="md"
+                    radius="0 50 50 0"
+                    component="a"
+                    href="https://www.fossuok.org/events/15"
+                    target="_blank"
+                  >
+                    Registrations - Open
+                  </Button>
+                </Timeline.Item>
+              </Timeline>
+            </Container>
+          </Group>
         </Container>
       </div>
+
       <div id="sponsors">
         <SponserCards
           cards={[{ title: "Our Sponsors", content: "", type: 1 }]}
