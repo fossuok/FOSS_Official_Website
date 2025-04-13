f6ab023e205d780881d88574024b88f6904333d0
pathummadhusanka <commits@pathummadhusanka.com>
2025-04-13 16:20:19 +0530

    chore: adjust speaker card structure for better readability

--- src/app/(pages)/project/2025/components/SpeakerCard.tsx
+++ src/app/(pages)/project/2025/components/SpeakerCard.tsx
@@ -3,38 +3,46 @@ import { IconBrandLinkedin } from "@tabler/icons-react";
 import classes from "../css/Cards.module.css";
 import Link from "next/link";
 interface SpeakerCardProps {
-  fname: string;
-  lname: string;
-  email: string;
-  position: string;
-  linkedin: string;
-  picture: string;
+	fname: string;
+	lname: string;
+	email: string;
+	position: string;
+	linkedin: string;
+	picture: string;
 }
 
 const SpeakerCard: React.FC<SpeakerCardProps> = ({
-  fname,
-  lname,
-  email,
-  position,
-  linkedin,
-  picture,
+	fname,
+	lname,
+	email,
+	position,
+	linkedin,
+	picture,
 }) => {
-  return (
-    <Paper radius="xl" p="lg" bg="transparent">
-      <Avatar src={picture} size={240} radius={30} mx="auto" />
+	return (
+		<Paper radius="xl" p="lg" bg="transparent">
+			<Avatar src={picture} size={240} radius={30} mx="auto" />
 
-      <Link href={linkedin} target="_blank" className={classes.linkedInIcon}>
-        <IconBrandLinkedin size={24} />
-      </Link>
+			<Link
+				href={linkedin}
+				target="_blank"
+				className={classes.linkedInIcon}
+			>
+				<IconBrandLinkedin size={24} />
+			</Link>
 
-      <Text pt={5} ta="left" fz="xl" fw={700}>
-        {fname + " " + lname}
-      </Text>
-      <Text ta="left" fz="sm" maw={240}>
-        {email + " • " + position}
-      </Text>
-    </Paper>
-  );
+			<Text pt={5} ta="left" fz="xl" fw={700}>
+				{fname + " " + lname}
+			</Text>
+			<Text ta="left" fz="sm" maw={240}>
+				{email} <br />
+				{/* TODO: Remove ternary (fname checking for TBA) after all speakers are added. 
+        Purpose of ternary is to remove 'at' between position and organization if speaker is empty. */}
+				{(fname === "TBA" ? "" : "at ") + position}
+				{/* {"at " + position} */}
+			</Text>
+		</Paper>
+	);
 };
 
 export default SpeakerCard;
